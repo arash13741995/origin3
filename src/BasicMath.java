14748276e19d112158d51685a209e3a8597ad125
arash13741995 <arash.beikmohammadi@gmail.com>
2017-07-29 17:46:51 +0430

    div

--- src/BasicMath.java
+++ src/BasicMath.java
@@ -1,19 +1,9 @@
 class BasicMath
 {
 
-<<<<<<< HEAD
     public int div(int a, int b)
     {
         return a / b;
-=======
-    public int mult(int a, int b)
-    {
-        return a * b;
-    }
-    public int sub(int a, int b)
-    {
-        return a - b;
->>>>>>> master
     }
 
     // TODO: subtract
