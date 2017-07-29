3df8f1144f337ad15c379b8d4d2af60ad9f38d7d
arash13741995 <arash.beikmohammadi@gmail.com>
2017-07-29 17:25:58 +0430

    sub

--- src/Test.java
+++ src/Test.java
@@ -9,16 +9,16 @@ class Test
     {
         int a = 5;
         int b = 6;
-        int expected = 11;
+        int expected = -1;
         BasicMath bm = new BasicMath();
-        int result = bm.sum(a, b);
+        int result = bm.sub(a, b);
         if (result == expected)
         {
-            System.out.println("Sum: OK");
+            System.out.println("Sub: OK");
         }
         else
         {
-            System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
+            System.out.println("Sub: Failed, expected = " + expected + ", result = " + result);
         }
     }
 }
